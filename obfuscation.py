194f8ae0d3f82c17e17e890643e8742d60de69fb
dontavious54 <118477725+dontavious54@users.noreply.github.com>
2024-03-17 21:53:43 +0000

    Update obfuscation.py

--- obfuscation.py
+++ obfuscation.py
@@ -1,30 +1,34 @@
-import random
 import os
+from Rules import Gen
+
+global gen1
+gen1 = Gen()
 
 class notObvs:
-    global key
-    
+        
     def __init__(self,file):
         self.file = file
-        with open("save.txt", "w") as f:
-            f.write(self.file)
-        fileP = open("save.txt","r")
-        self.FileN = fileP.read()
-        self.key = random.randint(1,10)
+        self.rule = gen1.id_generator()
+        self.key = gen1.alg(self.rule)
+        with open("save.txt","w") as x:
+            x.write(f"key:{self.rule}")
 
     def transfer(self):
-        tempWrite = open(self.FileN, "w")
-        tempWrite.write(f'from obfuscation import notObvs\nd1 = notObvs("{self.FileN}")\n\n')
-        tempWrite.write('d1.decode("""\n') 
+        tempWrite = open(self.file, "w")
+        tempWrite.write(f'from obfuscation import notObvs\nd1 = notObvs("{self.file}")\nd1.decode("""') 
+        
         with open("...txt", "r") as tempRead:
             for i in tempRead.read():
                 tempWrite.write(i)
-        tempWrite.write(f'""",{self.key})\nd1.run()')
+        tempWrite.write(f'""",)\nd1.run()')
+
+
         os.remove("...txt")
 
-    def encode(self):  
-        print(self.FileN) 
-        with open(self.FileN, "r") as file:
+
+
+    def encode(self): 
+        with open(self.file, "r") as file:
             for i in file.readlines():
                 for j in i:
                     if j != " " and j != "\n":
@@ -42,8 +46,16 @@ class notObvs:
     def run(self):
         os.system("python3 ...py") #change to "python ...py" if on windows
         os.remove("...py")
-        
-    def decode(self,string,key):   
+
+    def decode(self,string,rule="None"):
+        if rule == "None" :
+            rule = input("please enter a key: ")  
+
+        ordchra = 0
+        for i in rule:
+            ordchra +=ord(i)
+        key = round(ordchra//len(str(ordchra/3))/10)
+
         tempWrite = open("...py","w")
         for i in string:
             if i != " " and i != "\n":
@@ -52,22 +64,3 @@ class notObvs:
                 tempWrite.write(strchr)
             else:
                 tempWrite.write(i)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
