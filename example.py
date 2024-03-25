f42497556eb03dee411a0dd3bea4fa385d6262c1
dontavious54 <118477725+dontavious54@users.noreply.github.com>
2024-03-25 15:42:09 +0000

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from obfuscation import notObvs
 e1 = notObvs()
 
-#e1.encode("FILNAME","CASE:"py","txt")
-#e1.decode("FILNAME","CASE","key")
+#e1.encode("FILNAME",CASE:"py","txt")
+#e1.decode("FILNAME",CASE,"key)
