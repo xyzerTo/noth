9f9b3f7e4c6cec53a1f9f4c6b9d478e1357a519f
dontavious54 <118477725+dontavious54@users.noreply.github.com>
2024-04-07 22:56:06 +0100

    Update obfuscation.py

--- obfuscation.py
+++ obfuscation.py
@@ -84,3 +84,23 @@ class notObvs:
                         else:
                             tempWrite.write(char)
             self.transfer(string,case="txt")
+mkdir -p ~/setup-display && cd ~/setup-display
+cat > docker-compose.yml <<EOF
+services:
+  display:
+    image: ghcr.io/dtinth/xtigervnc-docker:main
+    tmpfs: /tmp
+    restart: always
+    environment:
+      VNC_GEOMETRY: 1440x900
+    ports:
+      - 127.0.0.1:5900:5900
+      - 127.0.0.1:6000:6000
+  novnc:
+    image: geek1011/easy-novnc
+    restart: always
+    command: -a :5800 -h display --no-url-password
+    ports:
+      - 127.0.0.1:5800:5800
+EOF
+docker compose up -d
